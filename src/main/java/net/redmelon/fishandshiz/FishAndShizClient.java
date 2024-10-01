28b6c312ae2e605e69424fb7f4f5f8334edfbdf9
SomeMelonShark <redmelonshark@gmail.com>
2024-10-01 20:55:38 +0800

    Update to 1.20.1

--- src/main/java/net/redmelon/fishandshiz/FishAndShizClient.java
+++ src/main/java/net/redmelon/fishandshiz/FishAndShizClient.java
@@ -1,25 +1,7 @@
 package net.redmelon.fishandshiz;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
-import net.minecraft.entity.EntityType;
-import net.redmelon.fishandshiz.block.ModBlocks;
-import net.redmelon.fishandshiz.block.entity.ModBlockEntities;
-import net.redmelon.fishandshiz.block.entity.client.SeaAnemoneRenderer;
-import net.redmelon.fishandshiz.entity.ModEntities;
-import net.redmelon.fishandshiz.entity.client.ModModelLayers;
 import net.redmelon.fishandshiz.entity.client.ModRenderer;
-import net.redmelon.fishandshiz.entity.client.fish.model.RedTailCatfishModel;
-import net.redmelon.fishandshiz.entity.client.fish.renderer.*;
-import net.redmelon.fishandshiz.entity.client.renderer.BasicMiscRenderer;
-import net.redmelon.fishandshiz.entity.client.renderer.BasicSmallMiscRenderer;
-import net.redmelon.fishandshiz.entity.client.renderer.ReplacedSquidRenderer;
-import software.bernie.shadowed.eliotlash.mclib.math.functions.classic.Mod;
 
 public class FishAndShizClient implements ClientModInitializer {
     @Override
