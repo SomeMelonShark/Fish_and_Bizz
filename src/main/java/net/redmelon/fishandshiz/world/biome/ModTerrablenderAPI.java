eb7f22b5dde57057750db9ffda1473a642e212a1
SomeMelonShark <redmelonshark@gmail.com>
2025-04-11 17:22:00 +0800

    Disabled Terrablender

--- src/main/java/net/redmelon/fishandshiz/world/biome/ModTerrablenderAPI.java
+++ src/main/java/net/redmelon/fishandshiz/world/biome/ModTerrablenderAPI.java
@@ -1,17 +1,10 @@
 package net.redmelon.fishandshiz.world.biome;
 
-import net.minecraft.util.Identifier;
-import net.redmelon.fishandshiz.FishAndShiz;
-import net.redmelon.fishandshiz.world.biome.surface.ModMaterialRules;
-import terrablender.api.Regions;
-import terrablender.api.SurfaceRuleManager;
-import terrablender.api.TerraBlenderApi;
-
-public class ModTerrablenderAPI implements TerraBlenderApi {
-    @Override
-    public void onTerraBlenderInitialized() {
-        Regions.register(new ModOverworldRegion(new Identifier(FishAndShiz.MOD_ID, "overworld"), 4));
-
-        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, FishAndShiz.MOD_ID, ModMaterialRules.makeRules());
-    }
-}
+//public class ModTerrablenderAPI implements TerraBlenderApi {
+//    @Override
+//    public void onTerraBlenderInitialized() {
+//        Regions.register(new ModOverworldRegion(new Identifier(FishAndShiz.MOD_ID, "overworld"), 4));
+//
+//        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, FishAndShiz.MOD_ID, ModMaterialRules.makeRules());
+//    }
+//}
